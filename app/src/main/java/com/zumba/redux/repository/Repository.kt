faf3c09c867e2e258828a09fdf61e7889ab0310e
Kuba <jakub.adamczewski@appunite.com>
2021-04-08 16:26:08 +0200

    progress couter flow

--- app/src/main/java/com/zumba/redux/repository/Repository.kt
+++ app/src/main/java/com/zumba/redux/repository/Repository.kt
@@ -7,14 +7,19 @@ data class Item(val id: Int, val name: String)
 
 object Repository {
     private val refresh = MutableSharedFlow<Unit>()
-    private val items: MutableList<Item> = (0..99).map { iterator -> Item(id = iterator, name = "Name $iterator") }.toMutableList()
-    
-    suspend fun getItems() = items.map { it.copy(name = "${it.name} ${System.currentTimeMillis()}") }
-    
-    fun getListFlow(): Flow<List<Item>> =
-        refresh.onStart { emit(Unit) }.flatMapLatest {
-            flow {emit(getItems())}
+    private val items: MutableList<Item> =
+        (0..99).map { iterator -> Item(id = iterator, name = "Name $iterator") }.toMutableList()
+
+    suspend fun getItems() =
+        items.map { it.copy(name = "${it.name} ${System.currentTimeMillis()}") }
+
+    fun getListFlow(): Flow<List<Item>> = refresh.onStart { emit(Unit) }.flatMapLatest {
+        flow {
+            delay(2000)
+            emit(getItems())
         }
+    }
+
 
     suspend fun removeItem(id: Int) {
         delay(1000)
--- app/src/main/java/com/zumba/redux/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/zumba/redux/viewmodel/ListViewModel.kt
@@ -1,16 +1,15 @@
 package com.zumba.redux.viewmodel
 
 import androidx.lifecycle.viewModelScope
-import com.zumba.redux.base.BasicError
-import com.zumba.redux.base.NoEffects
-import com.zumba.redux.base.ReduxViewModel
+import com.zumba.redux.base.*
 import com.zumba.redux.usecase.FlowGetItemsUseCase
 import com.zumba.redux.usecase.ViewItem
 import kotlinx.coroutines.launch
 
 data class ListState(
     val list: List<ViewItem> = listOf(),
-    val error: BasicError? = null
+    val error: BasicError? = null,
+    val progress: Boolean = false
 )
 
 class ListViewModel(private val getItemsUseCase: FlowGetItemsUseCase) :
@@ -20,11 +19,17 @@ class ListViewModel(private val getItemsUseCase: FlowGetItemsUseCase) :
 
     init {
         getData()
+
+        viewModelScope.launch {
+            progress.observeState
+                .collectAndSetState { progress -> copy(progress = progress) }
+        }
     }
 
     private fun getData() {
         viewModelScope.launch {
             getItemsUseCase(Unit)
+                .watchProgressOnEach(progress)
                 .collectAndSetState { result ->
                     result.fold(
                         { copy(error = it) },
